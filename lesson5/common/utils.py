07e0745ed4bc23c47648d5a001ac740210017a30
Onlyjoysprod <kujaku@yandex.ru>
2022-04-29 18:52:07 +0300

    Lesson5_HW_done

---
+++ lesson5/common/utils.py
@@ -0,0 +1,37 @@
+import json
+from common.variables import MAX_PACKAGE_LENGTH, ENCODING
+
+
+def decode_to_dict(byte_message):
+    try:
+        if isinstance(byte_message, bytes):
+            dict_message = byte_message.decode(ENCODING)
+            message = json.loads(dict_message)
+            return message
+        else:
+            raise TypeError
+    except TypeError:
+        print('Неверный тип данных')
+
+
+def encode_to_bytes(dict_message):
+    try:
+        if isinstance(dict_message, dict):
+            json_message = json.dumps(dict_message)
+            byte_message = json_message.encode(ENCODING)
+            return byte_message
+        else:
+            raise TypeError
+    except TypeError:
+        print('Неверный тип данных')
+
+
+def get_message(client):
+    bresponse = client.recv(MAX_PACKAGE_LENGTH)
+    response = decode_to_dict(bresponse)
+    return response
+
+
+def send_message(sock, message):
+    bpresence = encode_to_bytes(message)
+    sock.send(bpresence)
---
+++ lesson5/common/variables.py
@@ -0,0 +1,17 @@
+ACTION = 'action'
+TIME = 'time'
+USER = 'user'
+ACCOUNT_NAME = 'account_name'
+
+RESPONSE = 'response'
+PRESENCE = 'presence'
+ERROR = 'error'
+
+DEFAULT_IP_ADDRESS = '127.0.0.1'
+DEFAULT_SERVER_IP_ADDRESS = ''
+DEFAULT_PORT = 7777
+
+MAX_CONNECTIONS = 5
+MAX_PACKAGE_LENGTH = 1024
+
+ENCODING = 'utf-8'
