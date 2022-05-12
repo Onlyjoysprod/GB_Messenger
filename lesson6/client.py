2683e1560e60a9a5b70706d2e934013f9945ffb4
Onlyjoysprod <kujaku@yandex.ru>
2022-05-12 17:16:49 +0300

    Lesson6_HW_done

---
+++ lesson6/client.py
@@ -0,0 +1,67 @@
+import sys, json, time, argparse
+
+from socket import socket, AF_INET, SOCK_STREAM
+
+from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, RESPONSE, ERROR, \
+    DEFAULT_IP_ADDRESS, DEFAULT_PORT
+from common.utils import get_message, send_message
+from log.client_log_config import client_log
+from log.decorator_log import log
+
+parser = argparse.ArgumentParser(description='address and port')
+parser.add_argument('-a', dest="addr", default=DEFAULT_IP_ADDRESS)
+parser.add_argument('-p', dest="port", default=DEFAULT_PORT)
+args = parser.parse_args()
+
+
+@log
+def create_presence(account_name='Guest'):
+    out = {
+        ACTION: PRESENCE,
+        TIME: time.time(),
+        USER: {
+            ACCOUNT_NAME: account_name
+        }
+    }
+    return out
+
+
+@log
+def process_answer(message):
+    if RESPONSE in message:
+        if message[RESPONSE] == 200:
+            return '200 : OK'
+        else:
+            return f'400: {message[ERROR]}'
+    client_log.error('Wrong response')
+    raise ValueError
+
+
+@log
+def main():
+    try:
+        server_address = args.addr
+        server_port = int(args.port)
+        if server_port < 1024 or server_port > 65535:
+            client_log.error('Port can be in range 1024-6535')
+            raise ValueError
+    except ValueError:
+        sys.exit(1)
+
+    transport = socket(AF_INET, SOCK_STREAM)
+    transport.connect((server_address, server_port))
+    client_log.info(f'Connected to port: {server_port}')
+    message_to_server = create_presence()
+    send_message(transport, message_to_server)
+    client_log.info(f'Send message to server: {message_to_server}')
+
+    try:
+        answer = process_answer(get_message(transport))
+        client_log.info(f'Server response: {answer}')
+        print(answer)
+    except(ValueError, json.JSONDecodeError):
+        client_log.warning('Can not decode message')
+
+
+if __name__ == '__main__':
+    main()
---
+++ lesson6/server.py
@@ -0,0 +1,61 @@
+from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
+import sys
+import json
+import argparse
+from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, RESPONSE, ERROR, \
+    DEFAULT_SERVER_IP_ADDRESS, DEFAULT_PORT, MAX_CONNECTIONS
+from common.utils import get_message, send_message
+from log.server_log_config import server_log
+from log.decorator_log import log
+
+parser = argparse.ArgumentParser(description='address and port')
+parser.add_argument('-a', dest="addr", default=DEFAULT_SERVER_IP_ADDRESS)
+parser.add_argument('-p', dest="port", default=DEFAULT_PORT)
+args = parser.parse_args()
+
+
+@log
+def process_client_message(message):
+    if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
+            and USER in message and message[USER][ACCOUNT_NAME] == 'Guest':
+        return {RESPONSE: 200}
+    return {
+        RESPONSE: 400,
+        ERROR: 'Bad request'
+    }
+
+
+@log
+def main():
+    try:
+        listen_port = int(args.port)
+        listen_address = args.addr
+        if listen_port < 1024 or listen_port > 65535:
+            server_log.error('Port can be in range 1024-6535')
+            raise ValueError
+    except ValueError:
+        sys.exit(1)
+
+    transport = socket(AF_INET, SOCK_STREAM)
+    transport.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    transport.bind((listen_address, listen_port))
+    transport.listen(MAX_CONNECTIONS)
+
+    while True:
+        client, client_address = transport.accept()
+        server_log.info(f'Connected client: {client_address}')
+        try:
+            message_from_client = get_message(client)
+            server_log.info(f'Get message from client: {message_from_client}')
+            print(message_from_client)
+            response = process_client_message(message_from_client)
+            send_message(client, response)
+            server_log.info(f'Send response to client: {response}')
+            client.close()
+        except (ValueError, json.JSONDecodeError):
+            server_log.error('Got incorrect message from client')
+            client.close()
+
+
+if __name__ == '__main__':
+    main()
